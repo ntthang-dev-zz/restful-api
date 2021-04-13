6b4048c98bfe018ed090c019a5fe7cdc659d803b
ntthang-hcmut <ntthang.dev@gmail.com>
2021-04-13 15:35:09 +0700

    Fix bug

--- pkg/handler/todo_handler.go
+++ pkg/handler/todo_handler.go
@@ -98,3 +98,20 @@ func DeleteTodo(writer http.ResponseWriter, request *http.Request) {
 
 	responseWithJson(writer, http.StatusNotFound, map[string]string{"message": "Todo not found"})
 }
+
+func responseWithJson(writer http.ResponseWriter, status int, object interface{}) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(status)
+	json.NewEncoder(writer).Encode(object)
+}
+
+func generateId(todos []tdo.Todo) int {
+	var maxId int
+	for _, todo := range todos {
+		if todo.ID > maxId {
+			maxId = todo.ID
+		}
+	}
+
+	return maxId + 1
+}
